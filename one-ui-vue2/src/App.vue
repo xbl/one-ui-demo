d2b232e2e6fec9b0d93775e6df523999cbf6f24f
xbl <blxie@toughtworks.com>
2018-05-11 10:47:11 +0800

    init demo

---
+++ one-ui-vue2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <my-div v-show="show">
+      <button @click="hideDiv">abc</button>
+    </my-div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'app',
+  data() {
+    return {
+      show: true
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    hideDiv () {
+      this.show = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ one-ui-vue2/src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import App from './main.package'
+import 'one-ui'
+Vue.config.productionTip = false
+
+App.$mount('#app')
