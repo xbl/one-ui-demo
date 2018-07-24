564a54e47c8714addce233340adc62406ed72177
xbl <blxie@toughtworks.com>
2018-07-24 15:09:52 +0800

    update

--- one-ui-react/src/App.js
+++ one-ui-react/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <my-abc></my-abc>
+        <x-header></x-header>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
